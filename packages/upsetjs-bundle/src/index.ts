74fb303a80a5b3235ae60e2c68ed7f35fd52cf2e
Samuel Gratzl <sam@sgratzl.com>
2020-03-01 15:53:44 -0500

    try to fix setup

--- packages/upsetjs-bundle/src/index.ts
+++ packages/upsetjs-bundle/src/index.ts
@@ -1,5 +1,7 @@
-export { extractSets } from 'upsetjs';
 import { render, h } from 'preact';
+import UpSet from 'upsetjs';
+
+export { extractSets } from 'upsetjs';
 
 export function renderUpSet(node: HTMLElement, props: any) {
   render(h(UpSet, props), node);
