13101d5ec7dad7780256dd2ba7b813923f8a788f
Samuel Gratzl <sam@sgratzl.com>
2020-03-02 12:42:01 -0500

    better powerset tests

--- packages/model/src/powerSet.spec.ts
+++ packages/model/src/powerSet.spec.ts
@@ -1,17 +1,56 @@
 // import 'jest';
-import powerSet from './powerSet';
+import powerSet, { powerSetRecursive, powerSetNumber, PowerSetOptions } from './powerSet';
 
-test('basic', () => {
-  expect(Array.from(powerSet([] as number[]))).toStrictEqual([[]]);
-  expect(Array.from(powerSet([1]))).toStrictEqual([[], [1]]);
-  expect(Array.from(powerSet([1], { min: 1 }))).toStrictEqual([[1]]);
+function expectS(f: typeof powerSet, arg: number[], options: Partial<PowerSetOptions> = {}) {
+  return expect(JSON.stringify(Array.from(f(arg, options)).sort()));
+}
+
+test('[]', () => {
+  const arg: number[] = [];
+  const output = JSON.stringify([[]].sort());
+  const options: Partial<PowerSetOptions> = {};
+  expectS(powerSet, arg, options).toBe(output);
+  expectS(powerSetNumber, arg, options).toBe(output);
+  // expectS(powerSetBigInt, arg, options).toBe(output);
+  expectS(powerSetRecursive, arg, options).toBe(output);
 });
 
-test('2', () => {
-  expect(Array.from(powerSet([1, 2]))).toStrictEqual([[], [1], [2], [1, 2]]);
-  expect(Array.from(powerSet([1, 2], { min: 1, max: 1 }))).toStrictEqual([[1], [2]]);
+test('[] - min1', () => {
+  const arg: number[] = [];
+  const output = JSON.stringify([].sort());
+  const options: Partial<PowerSetOptions> = { min: 1 };
+  expectS(powerSet, arg, options).toBe(output);
+  expectS(powerSetNumber, arg, options).toBe(output);
+  // expectS(powerSetBigInt, arg, options).toBe(output);
+  expectS(powerSetRecursive, arg, options).toBe(output);
 });
 
-test('3', () => {
-  expect(Array.from(powerSet([1, 2, 3]))).toStrictEqual([[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]);
+test('[1,2]', () => {
+  const arg: number[] = [1, 2];
+  const output = JSON.stringify([[], [1], [2], [1, 2]].sort());
+  const options: Partial<PowerSetOptions> = {};
+  expectS(powerSet, arg, options).toBe(output);
+  expectS(powerSetNumber, arg, options).toBe(output);
+  // expectS(powerSetBigInt, arg, options).toBe(output);
+  expectS(powerSetRecursive, arg, options).toBe(output);
+});
+
+test('[1,2] min:1, max: 1', () => {
+  const arg: number[] = [1, 2];
+  const output = JSON.stringify([[1], [2]].sort());
+  const options: Partial<PowerSetOptions> = { min: 1, max: 1 };
+  expectS(powerSet, arg, options).toBe(output);
+  expectS(powerSetNumber, arg, options).toBe(output);
+  // expectS(powerSetBigInt, arg, options).toBe(output);
+  expectS(powerSetRecursive, arg, options).toBe(output);
+});
+
+test('[1,2,3]', () => {
+  const arg: number[] = [1, 2, 3];
+  const output = JSON.stringify([[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].sort());
+  const options: Partial<PowerSetOptions> = {};
+  expectS(powerSet, arg, options).toBe(output);
+  expectS(powerSetNumber, arg, options).toBe(output);
+  // expectS(powerSetBigInt, arg, options).toBe(output);
+  expectS(powerSetRecursive, arg, options).toBe(output);
 });
--- packages/model/src/powerSet.ts
+++ packages/model/src/powerSet.ts
@@ -1,11 +1,13 @@
-export default function* powerSet<T>(
+export declare type PowerSetOptions = {
+  min: number;
+  max: number;
+};
+
+export function* powerSetNumber<T>(
   arr: ReadonlyArray<T>,
   { min = 0, max = Infinity } = {}
 ): IterableIterator<ReadonlyArray<T>> {
   const total = 2 ** arr.length;
-
-  // just works till Number.MAX_SAFE_INTEGER
-
   let lastBit = 0;
   let lastBitAcc = 1;
   for (let i = 0; i < total; i++) {
@@ -29,4 +31,90 @@ export default function* powerSet<T>(
       yield sub;
     }
   }
+}
+
+export function* powerSetBigInt<T>(
+  arr: ReadonlyArray<T>,
+  { min = 0, max = Infinity } = {}
+): IterableIterator<ReadonlyArray<T>> {
+  const zero = BigInt(0);
+  const one = BigInt(1);
+  const two = BigInt(2);
+  const total = two << BigInt(arr.length);
+  let lastBit = 0;
+  let lastBitAcc = one;
+  const bits = arr.map((_, i) => one << BigInt(i));
+  for (let i = zero; i < total; i += one) {
+    if (i >= lastBitAcc) {
+      // just maximal goes up to this bit
+      // 0 ... 0 bit
+      // 1 ... 1 bit
+      // 2 ... 2 bits
+      // 4 ... 3 bits
+      lastBit++;
+      lastBitAcc = lastBitAcc << one;
+    }
+    const sub = [];
+    for (let j = 0; j < lastBit; j++) {
+      if ((i & bits[j]) != zero) {
+        // jth bit set in i
+        sub.push(arr[j]);
+      }
+    }
+    if (sub.length >= min && sub.length <= max) {
+      yield sub;
+    }
+  }
+}
+
+export function* powerSetRecursive<T>(
+  arr: ReadonlyArray<T>,
+  { min = 0, max = Infinity } = {}
+): IterableIterator<ReadonlyArray<T>> {
+  const check = (len: number) => len >= min && len <= max;
+  function* iter(subset: T[], start: number): IterableIterator<ReadonlyArray<T>> {
+    if (subset.length >= max) {
+      return;
+    }
+    for (let i = start; i < arr.length; i++) {
+      subset.push(arr[i]);
+      if (check(subset.length)) {
+        yield subset.slice();
+      }
+      const gen = iter(subset, i + 1);
+      let n = gen.next();
+      while (!n.done) {
+        yield n.value;
+        n = gen.next();
+      }
+      subset.pop();
+    }
+  }
+
+  if (check(0)) {
+    yield [];
+  }
+
+  const gen = iter([], 0);
+  let n = gen.next();
+  while (!n.done) {
+    yield n.value;
+    n = gen.next();
+  }
+}
+
+export default function powerSet<T>(
+  arr: ReadonlyArray<T>,
+  options: Partial<PowerSetOptions> = {}
+): IterableIterator<ReadonlyArray<T>> {
+  const total = Math.pow(2, arr.length);
+
+  if (total < Number.MAX_SAFE_INTEGER) {
+    return powerSetNumber(arr, options);
+  }
+  if (typeof window.BigInt !== 'undefined') {
+    return powerSetBigInt(arr, options);
+  }
+
+  return powerSetRecursive(arr, options);
 }
