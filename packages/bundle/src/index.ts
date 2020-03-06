f7ff7d8f29b94af151db78f15881dc65d574b1b5
Samuel Gratzl <sam@sgratzl.com>
2020-03-06 13:38:45 -0500

    prepare for release

--- packages/bundle/src/index.ts
+++ packages/bundle/src/index.ts
@@ -8,48 +8,26 @@ export function renderUpSet<T>(node: HTMLElement, props: UpSetProps<T>) {
   render(h(UpSetElement as any, props), node);
 }
 
-export class UpSet<T> extends Eventemitter implements Omit<UpSetProps<T>, 'onHover'> {
-  constructor(sets: ISets<T>, width: number, height: number) {
-    super();
-    this.parent = parent;
-  }
-  sets: import('../../model/dist').ISets<T>;
-  combinations?: import('../../model/dist').ISetCombinations<T> | undefined;
-  width: number;
-  height: number;
-  padding?: number | undefined;
-  barPadding?: number | undefined;
-  widthRatios?: [number, number, number] | undefined;
-  heightRatios?: [number, number] | undefined;
-  setName?;
-  combinationName?;
-  selectionColor?: string | undefined;
-  alternatingBackgroundColor?: string | undefined;
-  color?: string | undefined;
-  notMemberColor?: string | undefined;
-  labelStyle?;
-  setLabelStyle?;
-  setNameStyle?;
-  axisStyle?;
-  combinationNameStyle?;
-  triangleSize?: number | undefined;
-  className?: string | undefined;
-  style?;
+// export class UpSet<T> extends Eventemitter implements Omit<UpSetProps<T>, 'onHover'> {
+//   constructor(sets: ISets<T>, width: number, height: number) {
+//     super();
+//     this.parent = parent;
+//   }
 
-  update() {
-    this.render();
-  }
+//   update() {
+//     this.render();
+//   }
 
-  private render() {
-    render(
-      h(UpSetElement as any, {
-        ...this.props,
-        onClick: (s: ISetLike<T>) => this.emit('click', s),
-        onHover: (s: ISetLike<T> | null) => this.emit('hover', s),
-      }),
-      this.parent
-    );
-  }
-}
+//   private render() {
+//     render(
+//       h(UpSetElement as any, {
+//         ...this.props,
+//         onClick: (s: ISetLike<T>) => this.emit('click', s),
+//         onHover: (s: ISetLike<T> | null) => this.emit('hover', s),
+//       }),
+//       this.parent
+//     );
+//   }
+// }
 
-export default UpSet;
+// export default UpSet;
