2f03ddffa7bd0726f61e145aee1f8246a25a3890
Samuel Gratzl <sam@sgratzl.com>
2020-03-28 17:02:27 +0100

    try to fix script

--- netlify.sh
+++ netlify.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set -e
 
 rm -rf node_modules
 yarn set version berry
 yarn plugin import @yarnpkg/plugin-workspace-tools
 yarn config set checksumBehavior ignore
+
+echo "yarn install"
 yarn install --immutable
 
+echo "build things"
 yarn workspace @upsetjs/react run build-storybook
 yarn workspace @upsetjs/app run build
 
+echo "copy things"
 mkdir -p dist/api
 cp -r packages/app/dist/* dist/
 cp -r packages/react/storybook-static/* dist/api/
