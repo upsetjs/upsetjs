adab657b3f0b848afb9c4a3e69686b03f901edc1
Samuel Gratzl <sam@sgratzl.com>
2020-03-28 17:21:43 +0100

    try to improve script

--- netlify.sh
+++ netlify.sh
@@ -2,12 +2,15 @@
 set -e
 
 rm -rf node_modules
+rm -rf .yarnrc.yml
+rm -rf .yarn
+
 yarn set version berry
 yarn plugin import @yarnpkg/plugin-workspace-tools
 yarn config set checksumBehavior ignore
 
 echo "yarn install"
-yarn install --immutable
+yarn install --immutable --cache-folder $NETLIFY_BUILD_BASE/.yarn_cache
 
 echo "build things"
 yarn workspace @upsetjs/react run build-storybook
