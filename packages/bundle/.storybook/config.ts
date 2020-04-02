3e404cbc6172877d92e439bbc781d8d5b37e1965
Samuel Gratzl <sam@sgratzl.com>
2020-04-02 10:23:26 +0200

    remove storybook from bundle

--- packages/bundle/.storybook/config.ts
+++
@@ -1,4 +0,0 @@
-import { configure } from '@storybook/html';
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.(js|ts)x?$/), module);
