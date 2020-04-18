f39db1a96235d6bc247a597250a5a1f4ed66182e
Samuel Gratzl <sam@sgratzl.com>
2020-04-18 20:56:32 +0200

    restructure for reuse

---
+++ packages/react/sample/upset.js
@@ -0,0 +1,42 @@
+async function loadUpSet() {
+  document.body.style.backgroundColor = 'black';
+  document.body.insertAdjacentHTML('afterend', '<div id="app"></div>');
+
+  const react = document.createElement('script');
+  react.crossOrigin = true;
+  react.src = 'https://unpkg.com/react@16/umd/react.development.js';
+  document.head.appendChild(react);
+  await new Promise((resolve) => (react.onload = resolve));
+  const reactdom = document.createElement('script');
+  reactdom.crossOrigin = true;
+  reactdom.src = 'https://unpkg.com/react-dom@16/umd/react-dom.development.js';
+  document.head.appendChild(reactdom);
+  await new Promise((resolve) => (reactdom.onload = resolve));
+
+  window.UpSetJSModel = window.exports = {};
+
+  let s = document.createElement('script');
+  s.src = '../../model/dist/model.cjs.development.js';
+  document.body.appendChild(s);
+  await new Promise((resolve) => (s.onload = resolve));
+
+  window.require = function require(id) {
+    if (id === 'react') {
+      return window.React;
+    }
+    if (id === '@upsetjs/model') {
+      return window.UpSetJSModel;
+    }
+  };
+  window.UpSetJS = window.exports = {};
+
+  s = document.createElement('script');
+  s.src = '../dist/react.cjs.development.js';
+  document.body.appendChild(s);
+  await new Promise((resolve) => (s.onload = resolve));
+
+  function render(props) {
+    window.ReactDOM.render(window.React.createElement(window.UpSetJS.default, props), document.getElementById('app'));
+  }
+  return { UpSetJS: window.UpSetJS, render };
+}
