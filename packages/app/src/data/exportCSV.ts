a8bbc6dec837933887ea78d62aa4a0a95194a563
Samuel Gratzl <sam@sgratzl.com>
2020-04-19 16:51:31 +0200

    fix detect name

--- packages/app/src/data/exportCSV.ts
+++ packages/app/src/data/exportCSV.ts
@@ -109,8 +109,8 @@ export function importCSV(file: File): Promise<IDataSet> {
           description: `imported from ${file.name}`,
           attrs,
           load: () => {
-            const elems: IElems = results.data.map((e) => ({
-              name: e.name as string,
+            const elems: IElems = results.data.map((e, i) => ({
+              name: (e[nameAttr] as string) ?? i.toString(),
               sets: setNames.filter((f) => isTrue(e[f])),
               attrs: toAttrs(attrs, e),
             }));
