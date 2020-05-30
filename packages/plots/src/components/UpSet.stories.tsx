35efa6e476a34044ca8128650fdda661a06faf19
Samuel Gratzl <sam@sgratzl.com>
2020-05-30 14:39:00 +0200

    simple mcv

--- packages/plots/src/components/UpSet.stories.tsx
+++ packages/plots/src/components/UpSet.stories.tsx
@@ -6,7 +6,8 @@
  */
 
 import React, { useState } from 'react';
-import UpSetJS, { extractSets, ISetLike, UpSetProps } from '@upsetjs/react';
+import UpSetJS, { extractSets, UpSetSelection } from '@upsetjs/react';
+import Scatterplot from './Scatterplot';
 
 export default {
   title: 'UpSetJS Plot Addons',
@@ -15,32 +16,42 @@ export default {
 interface IElem {
   name: string;
   sets: string[];
-  value: number;
+  a: number;
+  b: number;
 }
 
-function InteractiveUpSet<T>(props: UpSetProps<T>) {
-  const [selection, setSelection] = useState(null as ISetLike<T> | null);
+const elems: IElem[] = [
+  { name: '1', sets: ['one', 'two', 'three'], a: Math.random(), b: Math.random() },
+  { name: '2', sets: ['one', 'two'], a: Math.random(), b: Math.random() },
+  { name: '3', sets: ['one'], a: Math.random(), b: Math.random() },
+  { name: '4', sets: ['two'], a: Math.random(), b: Math.random() },
+  { name: '5', sets: ['one', 'two', 'three'], a: Math.random(), b: Math.random() },
+  { name: '6', sets: ['three'], a: Math.random(), b: Math.random() },
+  { name: '7', sets: ['one', 'three'], a: Math.random(), b: Math.random() },
+  { name: '8', sets: ['one', 'three'], a: Math.random(), b: Math.random() },
+  { name: '9', sets: ['three'], a: Math.random(), b: Math.random() },
+  { name: '10', sets: ['two', 'three'], a: Math.random(), b: Math.random() },
+  { name: '11', sets: ['one'], a: Math.random(), b: Math.random() },
+  { name: '12', sets: ['one', 'three'], a: Math.random(), b: Math.random() },
+  { name: '13', sets: ['one', 'three'], a: Math.random(), b: Math.random() },
+];
+const sets = extractSets(elems);
 
-  return <UpSetJS selection={selection} onHover={setSelection} {...props} />;
-}
+export const UpSetScatterplot = () => {
+  const [selection, setSelection] = useState<UpSetSelection<IElem>>(null);
 
-export const Histogram = () => {
-  const elems: IElem[] = [
-    { name: '1', sets: ['one', 'two', 'three'], value: Math.random() },
-    { name: '2', sets: ['one', 'two'], value: Math.random() },
-    { name: '3', sets: ['one'], value: Math.random() },
-    { name: '4', sets: ['two'], value: Math.random() },
-    { name: '5', sets: ['one', 'two', 'three'], value: Math.random() },
-    { name: '6', sets: ['three'], value: Math.random() },
-    { name: '7', sets: ['one', 'three'], value: Math.random() },
-    { name: '8', sets: ['one', 'three'], value: Math.random() },
-    { name: '9', sets: ['three'], value: Math.random() },
-    { name: '10', sets: ['two', 'three'], value: Math.random() },
-    { name: '11', sets: ['one'], value: Math.random() },
-    { name: '12', sets: ['one', 'three'], value: Math.random() },
-    { name: '13', sets: ['one', 'three'], value: Math.random() },
-  ];
-  const sets = extractSets(elems);
-
-  return <InteractiveUpSet sets={sets} width={1200} height={500} />;
+  return (
+    <div>
+      <UpSetJS selection={selection} onHover={setSelection} sets={sets} width={1200} height={500} />
+      <Scatterplot
+        selection={selection}
+        onClick={setSelection}
+        width={500}
+        height={500}
+        xAttr="a"
+        yAttr="b"
+        elems={elems}
+      />
+    </div>
+  );
 };
