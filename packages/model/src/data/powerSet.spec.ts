4bc3e5289f547e0b65fa5e121701ed9fb40ad80f
Samuel Gratzl <sam@sgratzl.com>
2020-06-11 18:19:41 +0200

    add tests

--- packages/model/src/data/powerSet.spec.ts
+++ packages/model/src/data/powerSet.spec.ts
@@ -20,52 +20,54 @@ function expectS(f: typeof powerSetNumber, arg: number[], options: Partial<Power
   return expect(JSON.stringify(arr.sort()));
 }
 
-test('[]', () => {
-  const arg: number[] = [];
-  const output = JSON.stringify([[]].sort());
-  const options: Partial<PowerSetOptions> = {};
-  expectPowerSet(arg, options).toBe(output);
-  expectS(powerSetNumber, arg, options).toBe(output);
-  // expectS(powerSetBigInt, arg, options).toBe(output);
-  expectS(powerSetRecursive, arg, options).toBe(output);
-});
+describe('powerSet', () => {
+  test('[]', () => {
+    const arg: number[] = [];
+    const output = JSON.stringify([[]].sort());
+    const options: Partial<PowerSetOptions> = {};
+    expectPowerSet(arg, options).toBe(output);
+    expectS(powerSetNumber, arg, options).toBe(output);
+    // expectS(powerSetBigInt, arg, options).toBe(output);
+    expectS(powerSetRecursive, arg, options).toBe(output);
+  });
 
-test('[] - min1', () => {
-  const arg: number[] = [];
-  const output = JSON.stringify([].sort());
-  const options: Partial<PowerSetOptions> = { min: 1 };
-  expectPowerSet(arg, options).toBe(output);
-  expectS(powerSetNumber, arg, options).toBe(output);
-  // expectS(powerSetBigInt, arg, options).toBe(output);
-  expectS(powerSetRecursive, arg, options).toBe(output);
-});
+  test('[] - min1', () => {
+    const arg: number[] = [];
+    const output = JSON.stringify([].sort());
+    const options: Partial<PowerSetOptions> = { min: 1 };
+    expectPowerSet(arg, options).toBe(output);
+    expectS(powerSetNumber, arg, options).toBe(output);
+    // expectS(powerSetBigInt, arg, options).toBe(output);
+    expectS(powerSetRecursive, arg, options).toBe(output);
+  });
 
-test('[1,2]', () => {
-  const arg: number[] = [1, 2];
-  const output = JSON.stringify([[], [1], [2], [1, 2]].sort());
-  const options: Partial<PowerSetOptions> = {};
-  expectPowerSet(arg, options).toBe(output);
-  expectS(powerSetNumber, arg, options).toBe(output);
-  // expectS(powerSetBigInt, arg, options).toBe(output);
-  expectS(powerSetRecursive, arg, options).toBe(output);
-});
+  test('[1,2]', () => {
+    const arg: number[] = [1, 2];
+    const output = JSON.stringify([[], [1], [2], [1, 2]].sort());
+    const options: Partial<PowerSetOptions> = {};
+    expectPowerSet(arg, options).toBe(output);
+    expectS(powerSetNumber, arg, options).toBe(output);
+    // expectS(powerSetBigInt, arg, options).toBe(output);
+    expectS(powerSetRecursive, arg, options).toBe(output);
+  });
 
-test('[1,2] min:1, max: 1', () => {
-  const arg: number[] = [1, 2];
-  const output = JSON.stringify([[1], [2]].sort());
-  const options: Partial<PowerSetOptions> = { min: 1, max: 1 };
-  expectPowerSet(arg, options).toBe(output);
-  expectS(powerSetNumber, arg, options).toBe(output);
-  // expectS(powerSetBigInt, arg, options).toBe(output);
-  expectS(powerSetRecursive, arg, options).toBe(output);
-});
+  test('[1,2] min:1, max: 1', () => {
+    const arg: number[] = [1, 2];
+    const output = JSON.stringify([[1], [2]].sort());
+    const options: Partial<PowerSetOptions> = { min: 1, max: 1 };
+    expectPowerSet(arg, options).toBe(output);
+    expectS(powerSetNumber, arg, options).toBe(output);
+    // expectS(powerSetBigInt, arg, options).toBe(output);
+    expectS(powerSetRecursive, arg, options).toBe(output);
+  });
 
-test('[1,2,3]', () => {
-  const arg: number[] = [1, 2, 3];
-  const output = JSON.stringify([[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].sort());
-  const options: Partial<PowerSetOptions> = {};
-  expectPowerSet(arg, options).toBe(output);
-  expectS(powerSetNumber, arg, options).toBe(output);
-  // expectS(powerSetBigInt, arg, options).toBe(output);
-  expectS(powerSetRecursive, arg, options).toBe(output);
+  test('[1,2,3]', () => {
+    const arg: number[] = [1, 2, 3];
+    const output = JSON.stringify([[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].sort());
+    const options: Partial<PowerSetOptions> = {};
+    expectPowerSet(arg, options).toBe(output);
+    expectS(powerSetNumber, arg, options).toBe(output);
+    // expectS(powerSetBigInt, arg, options).toBe(output);
+    expectS(powerSetRecursive, arg, options).toBe(output);
+  });
 });
