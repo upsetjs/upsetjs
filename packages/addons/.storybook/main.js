6838a8967f1a7a85197429075b3ac6ee9815bb40
Samuel Gratzl <sam@sgratzl.com>
2020-06-15 10:26:43 +0200

    fix storybook

--- packages/addons/.storybook/main.js
+++ packages/addons/.storybook/main.js
@@ -1,16 +1,6 @@
-const path = require('path');
-
 module.exports = {
-  stories: ['../src/**/*.stories.(ts|tsx)'],
-  addons: [
-    {
-      name: require.resolve('@storybook/preset-typescript'),
-      options: {
-        include: [path.resolve(__dirname, '../src')],
-      },
-    },
-    require.resolve('@storybook/addon-docs/preset'),
-  ],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
+  addons: [require.resolve('@storybook/addon-docs/preset')],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.tsx?$/,
