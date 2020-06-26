de3c437daf882238d44b00929d8c919343d79495
Samuel Gratzl <sam@sgratzl.com>
2020-06-26 19:44:57 +0200

    add computed overlap function

--- packages/model/src/data/extractFromExpression.ts
+++ packages/model/src/data/extractFromExpression.ts
@@ -5,10 +5,19 @@
  * Copyright (c) 2020 Samuel Gratzl <sam@sgratzl.com>
  */
 
-import { ISets, SetCombinationType, ISet, ISetCombination } from '../model';
+import {
+  ISets,
+  SetCombinationType,
+  ISet,
+  ISetCombination,
+  ISetLike,
+  toKey as toDefaultKey,
+  ISetCombinations,
+} from '../model';
 import { SortSetOrder, postprocessSets } from './asSets';
 import { SortCombinationOrder, SortCombinationOrders, postprocessCombinations } from './asCombinations';
 import { SET_JOINERS } from './constants';
+import { isSet } from '../validators';
 
 export interface ExtractFromExpressionOptions {
   type?: SetCombinationType;
@@ -18,7 +27,202 @@ export interface ExtractFromExpressionOptions {
   combinationOrder?: SortCombinationOrder | SortCombinationOrders;
 
   joiner?: string;
+  /**
+   * optional function to identify the same sets
+   * @param set the set to generate a key for
+   */
+  toKey?: (set: ISetLike<unknown>) => string;
 }
+
+interface CommonInfo {
+  done: null | number;
+  aKey: string;
+  bKey: string;
+  aIsSet: boolean;
+  bIsSet: boolean;
+  toKey: (set: ISetLike<unknown>) => string;
+}
+
+/**
+ * common compare helper
+ * ! when done is set i.e., not null there is no guarantee for any other value
+ * @param a
+ * @param b
+ * @param toKey
+ */
+export function common<T>(a: ISetLike<T>, b: ISetLike<T>, toKey = toDefaultKey) {
+  const r: CommonInfo = {
+    done: null,
+    aKey: '',
+    bKey: '',
+    aIsSet: false,
+    bIsSet: false,
+    toKey,
+  };
+  if (a === b) {
+    r.done = a.cardinality;
+    return r;
+  }
+  if (a.cardinality === 0 || b.cardinality === 0) {
+    r.done = 0;
+    return r;
+  }
+  r.aKey = toKey(a);
+  r.bKey = toKey(b);
+  if (r.aKey === r.bKey) {
+    r.done = a.cardinality;
+    return r;
+  }
+  r.aIsSet = isSet(a);
+  r.bIsSet = isSet(b);
+
+  // cannot decide yet
+  return r;
+}
+
+export function aInB<T>(b: ISetLike<T>, r: CommonInfo) {
+  if (r.bIsSet || !r.aIsSet) {
+    return false;
+  }
+  return Array.from((b as ISetCombination<T>).sets)
+    .map(r.toKey)
+    .includes(r.aKey);
+}
+
+export function bInA<T>(a: ISetLike<T>, r: CommonInfo) {
+  if (!r.bIsSet || r.aIsSet) {
+    return false;
+  }
+  return Array.from((a as ISetCombination<T>).sets)
+    .map(r.toKey)
+    .includes(r.bKey);
+}
+
+export function keyedCombinations<T>(combinations: ISetCombinations<T>, toKey = toDefaultKey) {
+  return combinations.map((c) => {
+    const s = Array.from(c.sets).map(toKey).sort();
+    return {
+      key: s.join('&'),
+      s,
+      sets: new Set(s),
+      degree: c.degree,
+      cardinality: c.cardinality,
+    };
+  });
+}
+
+export function combinedKey<T>(a: ISetLike<T>, b: ISetLike<T>, r: CommonInfo) {
+  const sets = new Set<string>();
+  if (r.aIsSet) {
+    sets.add(r.aKey);
+  } else {
+    for (const s of Array.from((a as ISetCombination<T>).sets)) {
+      sets.add(r.toKey(s));
+    }
+  }
+  if (r.bIsSet) {
+    sets.add(r.bKey);
+  } else {
+    for (const s of Array.from((b as ISetCombination<T>).sets)) {
+      sets.add(r.toKey(s));
+    }
+  }
+  return Array.from(sets).sort().join('&');
+}
+
+export function generateDistinctOverlapFunction<T>(combinations: ISetCombinations<T>, toKey = toDefaultKey) {
+  const combinationsBySet = new Map<string, { cardinality: number; sets: Set<string> }[]>();
+  for (const c of keyedCombinations(combinations, toKey)) {
+    for (const s of c.s) {
+      if (combinationsBySet.has(s)) {
+        combinationsBySet.get(s)!.push(c);
+      } else {
+        combinationsBySet.set(s, [c]);
+      }
+    }
+  }
+
+  return (a: ISetLike<T>, b: ISetLike<T>) => {
+    const r = common(a, b, toKey);
+    if (r.done != null) {
+      return r.done;
+    }
+    if (!r.aIsSet && !r.bIsSet) {
+      // by definition combinations are distinct thus if not a set in involved they overlap is 0
+      return 0;
+    }
+    if (r.aIsSet && !r.bIsSet) {
+      // if a is a set and a subset -> subset else no overlap since distinct
+      return aInB(b, r) ? b.cardinality : 0;
+    }
+    if (!r.aIsSet && r.bIsSet) {
+      // if a is a set and a subset -> subset else no overlap since distinct
+      return bInA(a, r) ? a.cardinality : 0;
+    }
+    // both are sets solution are all combinations which have both in their key
+    // e.g. A&B = A&B + A&B&C
+    const hasA = combinationsBySet.get(r.aKey);
+    const hasB = combinationsBySet.get(r.bKey);
+    if (!hasA || !hasB) {
+      return 0;
+    }
+    if (hasA.length < hasB.length) {
+      return hasA.reduce((acc, c) => acc + (c.sets.has(r.bKey) ? c.cardinality : 0), 0);
+    }
+    return hasB.reduce((acc, c) => acc + (c.sets.has(r.aKey) ? c.cardinality : 0), 0);
+  };
+}
+
+export function generateIntersectionOverlapFunction<T>(combinations: ISetCombinations<T>, toKey = toDefaultKey) {
+  const combinationsByKey = new Map(keyedCombinations(combinations, toKey).map((d) => [d.key, d.cardinality]));
+
+  return (a: ISetLike<T>, b: ISetLike<T>) => {
+    const r = common(a, b, toKey);
+    if (r.done != null) {
+      return r.done;
+    }
+    if (r.aIsSet && !r.bIsSet && aInB(b, r)) {
+      // own subset thus all of the intersection
+      return b.cardinality;
+    }
+    if (!r.aIsSet && r.bIsSet && bInA(a, r)) {
+      // own subset thus all of the intersection
+      return a.cardinality;
+    }
+    const key = combinedKey(a, b, r);
+    if (!combinationsByKey.has(key)) {
+      return 0; // cannot compute
+    }
+    // get A&B
+    return combinationsByKey.get(key)!;
+  };
+}
+
+export function generateUnionOverlapFunction<T>(combinations: ISetCombinations<T>, toKey = toDefaultKey) {
+  const combinationsByKey = new Map(keyedCombinations(combinations, toKey).map((d) => [d.key, d.cardinality]));
+
+  return (a: ISetLike<T>, b: ISetLike<T>) => {
+    const r = common(a, b, toKey);
+    if (r.done != null) {
+      return r.done;
+    }
+    if (r.aIsSet && !r.bIsSet && aInB(b, r)) {
+      // own subset thus all of the main set, since with union it can just grow
+      return a.cardinality;
+    }
+    if (!r.aIsSet && r.bIsSet && bInA(a, r)) {
+      // own subset thus all of the main set, since with union it can just grow
+      return b.cardinality;
+    }
+    const key = combinedKey(a, b, r);
+    if (!combinationsByKey.has(key)) {
+      return 0; // cannot compute
+    }
+    //A&B = A+B - AUB
+    return a.cardinality + b.cardinality - combinationsByKey.get(key)!;
+  };
+}
+
 /**
  * extract sets and combinations out of a given list of static combination information.
  * In addition an accessor is used to specify the set names
@@ -56,6 +260,15 @@ export default function extractFromExpression<T extends { value: number }>(
   const sets: ISet<unknown>[] = [];
   const setLookup = new Map<string, ISet<unknown>>();
 
+  let overlapFunction: null | ((a: ISetLike<unknown>, b: ISetLike<unknown>) => number) = null;
+
+  function overlap(this: ISetLike<unknown>, that: ISetLike<unknown>) {
+    if (this === that) {
+      return this.cardinality;
+    }
+    return overlapFunction == null ? 0 : overlapFunction(this, that);
+  }
+
   const cs: (T & ISetCombination<unknown>)[] = combinations.map((c) => {
     const containedSets = acc(c);
 
@@ -68,6 +281,7 @@ export default function extractFromExpression<T extends { value: number }>(
         elems: [],
         name: s,
         type: 'set',
+        overlap,
       };
       sets.push(set);
       setLookup.set(set.name, set);
@@ -90,11 +304,13 @@ export default function extractFromExpression<T extends { value: number }>(
       );
     }
 
+    const name = containedSets.join(joiner);
     return Object.assign(
       {
         type,
         elems: [],
-        name: containedSets.join(joiner),
+        name,
+        overlap,
       },
       c,
       {
@@ -108,10 +324,25 @@ export default function extractFromExpression<T extends { value: number }>(
   const sortedSets = postprocessSets(sets, {
     order: options.setOrder,
   });
+  const sortedCombinations = postprocessCombinations(sortedSets, cs, {
+    order: options.combinationOrder,
+  });
+
+  // no since we are done we can compute a proper overlap with the given data
+  switch (type) {
+    case 'distinctIntersection':
+      overlapFunction = generateDistinctOverlapFunction(sortedCombinations, options.toKey);
+      break;
+    case 'union':
+      overlapFunction = generateUnionOverlapFunction(sortedCombinations, options.toKey);
+      break;
+    default:
+      overlapFunction = generateIntersectionOverlapFunction(sortedCombinations, options.toKey);
+      break;
+  }
+
   return {
     sets: sortedSets,
-    combinations: postprocessCombinations(sortedSets, cs, {
-      order: options.combinationOrder,
-    }),
+    combinations: sortedCombinations,
   };
 }
