50fa4e4ee25a239abb13da252e4933dce9ca7fd9
Samuel Gratzl <sam@sgratzl.com>
2020-07-28 13:59:27 +0200

    feat:use @sgratzl/boxplot lib

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -5,7 +5,8 @@
  * Copyright (c) 2020 Samuel Gratzl <sam@sgratzl.com>
  */
 
-export { default as boxplot, BoxplotStatsOptions, CustomQuantileMethod, IBoxPlot, QuantilesMethod } from './boxplot';
+export { default as boxplot } from '@sgratzl/boxplots';
+export * from '@sgratzl/boxplots';
 export { rand } from './rand';
 export { default as normalize, denormalize } from './normalize';
 export { ICategory, ICategoryBin, categoricalHistogram, ICategories, ICategoryBins } from './categorical';
