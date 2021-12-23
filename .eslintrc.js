981e3be9c584fbded8b515f0b3e3a42049162e90
Samuel Gratzl <sam@sgratzl.com>
2021-12-23 16:15:43 -0500

    build: fix playground build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 const pkg = require('./package.json');
 
 module.exports = {
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['react-app', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
+  extends: ['react-app', 'plugin:prettier/recommended', 'prettier'],
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
